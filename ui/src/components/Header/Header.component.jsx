1f2d17bd04e2e6cbb79e5f045271b371c260d8c9
rvitality <vital.renzjohn@gmail.com>
2023-04-01 20:35:44 +0800

    feat: add header component

---
+++ ui/src/components/Header/Header.component.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <div class="bg-amber-400 py-4 px-14 flex justify-between items-center">
+                    <div class="flex items-center justify-center gap-6">
+                        <Link to="/">
+                            <img
+                                src="https://ateressi.sirv.com/Untitled%20design.png"
+                                height="100"
+                                width="80"
+                                alt="Logo"
+                                class="mr-2"
+                            />
+                        </Link>
+
+                        <nav>
+                            <Link to="feed" class="inline-block p-2 amber-700 hover:text-amber-700">
+                                Feed
+                            </Link>
+                            <Link
+                                to="Volunteer"
+                                class="inline-block p-2 amber-700 hover:text-amber-700"
+                            >
+                                Volunteer
+                            </Link>
+                        </nav>
+                    </div>
+
+                    <div class="flex items-center justify-center gap-4">
+                        <Link to="login" class="inline-block p-2  hover:text-indigo-100 mr-2">
+                            Log In
+                        </Link>
+                        <Link to="signup" class="bg-white rounded-full py-3 px-5 ">
+                            Sign Up
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
