3012f875f214f62d86999ad045aaa3a9e3ea4ee4
rvitality <vital.renzjohn@gmail.com>
2023-04-02 10:27:37 +0800

    style: add new font family

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         extend: {
             fontFamily: {
                 sans: ["Bai Jamjuree", "sans-serif"],
+                satisfy: ["Bangers", "cursive"],
             },
         },
     },
